d778f2d8cf45c58340f1aed75c0e4f0afa334c43
balamurugan97 <balamurugan97@users.noreply.github.com>
2021-12-27 22:09:57 +0530

    Add files via upload

---
+++ ticketbase/src/main/java/com/bookmyaction/tickettakie/ticketbase/TicketbaseApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyaction.tickettakie.ticketbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbaseApplication.class, args);
+	}
+
+}
---
+++ ticketbase/src/test/java/com/bookmyaction/tickettakie/ticketbase/TicketbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyaction.tickettakie.ticketbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
